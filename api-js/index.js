e8a16bcbc4c450769f889415d5792b20f65624b9
BelmontRafael <rafaelmagalhaes12323@gmail.com>
2026-04-08 22:35:36 -0300

    implementando dockerfile

--- api-js/index.js
+++ api-js/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import router from './router.js';
 import db from './database.js';
+import './modelIndex.js';
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,7 @@ app.use('/', router);
 
 try {
     await db.authenticate();
+    await db.sync();
     console.log("Eba!");
 } catch (error) {
     console.log("Não eba...");
